756162e197d46f0d1c833bf22611ee94678ac8f1
abdulkarim1997 <abdulkarimsalem1997@gmail.com>
2023-10-25 11:18:59 +0300

    make routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PassportAuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post("register", [PassportAuthController::class, 'register']);
+Route::post("login", [PassportAuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api'])->group(function () {
+
+    Route::get("userinfo", [PassportAuthController::class, 'userInfo']);
+    Route::resource("products", ProductController::class);
 });
+
+
+
